438cebd5999f424ca76272e488803172361e610c
gzr7702 <rgraziano.gzr7702@gmail.com>
2017-05-27 17:21:42 -0400

    refactor: onClick in main activity

--- app/src/main/java/com/gzr7702/nyctourguide/LocationItem.java
+++ app/src/main/java/com/gzr7702/nyctourguide/LocationItem.java
@@ -19,23 +19,11 @@ class LocationItem {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public int getImage() {
         return image;
-    }
-
-    public void setImage(int image) {
-        this.image = image;
     }
 }
--- app/src/main/java/com/gzr7702/nyctourguide/MainActivity.java
+++ app/src/main/java/com/gzr7702/nyctourguide/MainActivity.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // Get all Buttons
         Button restaurantButton = (Button) findViewById(R.id.restaurant_button);
@@ -19,33 +20,31 @@ public class MainActivity extends AppCompatActivity {
         Button entertainmentButton = (Button) findViewById(R.id.entertainment_button);
         Button hotelButton = (Button) findViewById(R.id.hotel_button);
 
-        // Set up callbacks
-        restaurantButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), RestaurantActivity.class);
-                startActivity(intent);
-            }
-        });
+        restaurantButton.setOnClickListener(this);
+        sightsButton.setOnClickListener(this);
+        entertainmentButton.setOnClickListener(this);
+        hotelButton.setOnClickListener(this);
+    }
 
-        sightsButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SightsActivity.class);
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.restaurant_button:
+                intent = new Intent(getApplicationContext(), RestaurantActivity.class);
                 startActivity(intent);
-            }
-        });
-
-        entertainmentButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), EntertainmentActivity.class);
+                break;
+            case R.id.sights_button:
+                intent = new Intent(getApplicationContext(), SightsActivity.class);
                 startActivity(intent);
-            }
-        });
-
-        hotelButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), HotelActivity.class);
+                break;
+            case R.id.entertainment_button:
+                intent = new Intent(getApplicationContext(), EntertainmentActivity.class);
                 startActivity(intent);
-            }
-        });
+                break;
+            case R.id.hotel_button:
+                intent = new Intent(getApplicationContext(), EntertainmentActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
